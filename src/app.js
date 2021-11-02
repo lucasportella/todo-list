7b32cb3b18dda2876221e5b819abf6344126ac3c
lucas <lucasportellaagu@gmail.com>
2021-11-01 21:44:37 -0300

    change cors policy to allow cross origin requests between localhosts ports

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const tasksRoute = require('./routes/tasksRoute');
 
 const app = express();
 
 app.use(express.json());
+
+// regex pattern in origin to fit cors policy, this allows communication between any localhost ports
+app.use(cors({
+  origin: /http:\/\/localhost:/,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 app.use('/tasks', tasksRoute);
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,6 @@
-const cors = require('cors');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-
-app.use(cors({
-  origin: `http://localhost:${PORT}`,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
