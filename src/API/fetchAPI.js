56ae9c1c0b427929ac68be45f2696d89d82808e1
lucas <lucasportellaagu@gmail.com>
2021-11-02 16:55:48 -0300

    postTask start

--- src/API/fetchAPI.js
+++ src/API/fetchAPI.js
@@ -2,4 +2,10 @@ import axios from 'axios';
 
 const getTasks = async () => axios.get('http://localhost:3000/tasks/');
 
+// const postTask = async (newTask) => {
+//     axios.post('http://localhost:3000/tasks/', {
+
+//     })
+// };
+
 export default getTasks;
