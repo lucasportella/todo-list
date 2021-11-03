ea43a8ab3dbf97fba28e734b958e692383ad1c98
lucas <lucasportellaagu@gmail.com>
2021-11-02 21:03:25 -0300

    create remote task API function

--- src/API/fetchAPI.js
+++ src/API/fetchAPI.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
 import removeUnderscore from '../utils/removeUnderscore';
 
+const baseEndPoint = 'http://localhost:3000/tasks/';
+
 const getTasks = async () => {
-  const result = await axios.get('http://localhost:3000/tasks/');
+  const result = await axios.get(baseEndPoint);
   return removeUnderscore(result.data);
 };
 
 const postTask = async (newTask) => {
-  const result = await axios.post('http://localhost:3000/tasks/', newTask);
+  const result = await axios.post(baseEndPoint, newTask);
   if (result.status === 201) {
     return true;
   }
   return false;
 };
 
-export { getTasks, postTask };
+const removeTask = async (id) => {
+  const result = await axios.delete(baseEndPoint, { data: { id } });
+  if (result.status === 200) {
+    return true;
+  }
+  return false;
+};
+
+export { getTasks, postTask, removeTask };
