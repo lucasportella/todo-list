b14c767becb1f2aac3109fbc9d0eb31ab7067e5a
lucas <lucasportellaagu@gmail.com>
2021-11-03 21:28:30 -0300

    remote cors test

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const tasksRoute = require('./routes/tasksRoute');
 
 const app = express();
@@ -7,10 +7,10 @@ const app = express();
 app.use(express.json());
 
 // regex pattern in origin to fit cors policy, this allows communication between any localhost ports
-app.use(cors({
-  origin: 'https://mern-todo-list-backend.herokuapp.com/',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+// app.use(cors({
+//   origin: 'https://mern-todo-list-backend.herokuapp.com/',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
 
 app.use('/tasks', tasksRoute);
 
