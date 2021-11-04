070660eba9bd3c377e59923261c0f1b0823e3282
lucas <lucasportellaagu@gmail.com>
2021-11-03 21:12:14 -0300

    cors enpoint fix test

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // regex pattern in origin to fit cors policy, this allows communication between any localhost ports
 app.use(cors({
-  origin: /http:\/\/localhost:/,
+  origin: '/https://mern-todo-list-backend.herokuapp.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
