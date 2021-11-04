f3e9fc735b5ad77f9be5edc2c577b84102be3b3a
lucas <lucasportellaagu@gmail.com>
2021-11-03 23:30:40 -0300

    update fetch endpoint to deployed API

--- src/API/fetchAPI.js
+++ src/API/fetchAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import removeUnderscore from '../utils/removeUnderscore';
 
-const baseEndpoint = 'http://localhost:3000/';
+const baseEndpoint = 'https://mern-todo-list-backend.herokuapp.com/';
 let endpoint = baseEndpoint;
 
 const endpointTransporter = (endpointAttach) => {
