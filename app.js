67a78f242c994aba66abd7e404832cfd948c3f9e
lucas <lucasportellaagu@gmail.com>
2021-11-04 23:23:20 -0300

    allow localhost again for dev branch

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // regex pattern in origin to fit cors policy, this allows communication between any localhost ports
 app.use(cors({
-  origin: 'https://mern-todo-list-frontend.herokuapp.com',
+  origin: ['https://mern-todo-list-frontend.herokuapp.com', /http:\/\/localhost:/],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
