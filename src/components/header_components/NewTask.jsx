7531bc2628ec5eb51a04f1f7067c88508663858b
lucas <lucasportellaagu@gmail.com>
2022-02-25 21:36:13 -0300

    linter and final buttons reduce dize

--- src/components/header_components/NewTask.jsx
+++ src/components/header_components/NewTask.jsx
@@ -1,13 +1,17 @@
-import React, { useState, useContext } from 'react';
-import { postTask } from '../../API/fetchAPI';
-import TasksContext from '../../context/TasksContext';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { useState, useContext } from "react";
+import { postTask } from "../../API/fetchAPI";
+import TasksContext from "../../context/TasksContext";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const NewTask = () => {
-  const { updateTasks, setAlertMessage, alertMessageReset } = useContext(TasksContext);
+  const { updateTasks, setAlertMessage, alertMessageReset } =
+    useContext(TasksContext);
   const [newTaskMode, setNewTaskMode] = useState(false);
-  const [newTask, setNewTask] = useState({ status: 'pending', task: 'check emails' });
+  const [newTask, setNewTask] = useState({
+    status: "pending",
+    task: "check emails",
+  });
 
   const handleClick = () => setNewTaskMode(!newTaskMode);
 
@@ -18,10 +22,10 @@ const NewTask = () => {
   const handleClickAndSubmit = async () => {
     try {
       await postTask(newTask);
-      setAlertMessage('New task created!');
+      setAlertMessage("New task created!");
       await updateTasks();
     } catch (error) {
-      setAlertMessage('Failed to create new task.');
+      setAlertMessage("Failed to create new task.");
     } finally {
       setNewTaskMode(!newTaskMode);
       alertMessageReset();
@@ -29,15 +33,23 @@ const NewTask = () => {
   };
 
   const renderNewTaskButton = () => (
-    <span><Button size="sm" type="button" onClick={handleClick}>New Task</Button></span>
+    <span>
+      <Button size="sm" type="button" onClick={handleClick}>
+        New Task
+      </Button>
+    </span>
   );
 
   const renderNewTaskMode = () => (
-
     <div>
       <Form.Group>
-      Task:
-        <Form.Control as="select" onChange={handleChange} name="task" id="dropdown-task">
+        Task:
+        <Form.Control
+          as="select"
+          onChange={handleChange}
+          name="task"
+          id="dropdown-task"
+        >
           <option value="check emails">Check emails</option>
           <option value="take dog for a walk ">Take dog for a walk</option>
           <option value="wash the dishes">Wash the dishes</option>
@@ -51,18 +63,26 @@ const NewTask = () => {
       </Form.Group>
       <div>
         Task status:
-        <Form.Control as="select" onChange={handleChange} name="status" id="dropdown-status">
+        <Form.Control
+          as="select"
+          onChange={handleChange}
+          name="status"
+          id="dropdown-status"
+        >
           <option value="pending">Pending</option>
           <option value="in progress">In progress</option>
           <option value="done">Done</option>
         </Form.Control>
       </div>
       <span>
-        <Button type="button" onClick={handleClickAndSubmit}>Confirm</Button>
-        <Button type="button" onClick={handleClick}>Cancel</Button>
+        <Button type="button" size="sm" onClick={handleClickAndSubmit}>
+          Confirm
+        </Button>
+        <Button type="button" size="sm" onClick={handleClick}>
+          Cancel
+        </Button>
       </span>
     </div>
-
   );
 
   return (
